14aef5f00a58c5be42bb320e580ccda7e42730f4
Viktor Tasevski <viktor-tasevski@hotmail.com>
2024-09-11 19:20:22 +0200

    updates

---
+++ public/config/defaultUser.js
@@ -0,0 +1,10 @@
+export const defaultUser = {
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    parkingZones: ['NONE'],
+    mobile: '',
+    status: 'NOT_WORKING',
+    locked: true,
+};
