276cfefe2faf6913698a713d775c4452f6f6fb01
Viktor Tasevski <viktor-tasevski@hotmail.com>
2024-09-11 19:20:06 +0200

    updates

---
+++ public/config/enums.js
@@ -0,0 +1,24 @@
+export const employeeStatus = {
+    NOT_WORKING: 'Не работи',
+    WORKING: 'Работи',
+    VACATION: 'На одмор',
+    PAID_LEAVE: 'На боледување',
+};
+
+export const accountStatus = {
+    enabled: 'Активен',
+    disabled: 'Неактивен',
+};
+
+export const sessionStatus = {
+    idle: 'STARTED_UNVERIFIED',
+    active: 'STARTED_VERIFIED',
+    over: 'ENDED_UNPAID',
+};
+
+export const roles = {
+    user: 'ROLE_REG_USER', 
+    guest: 'ROLE_USER',
+    employee: 'ROLE_WORKER',
+    admin: 'ROLE_ADMIN',
+};
