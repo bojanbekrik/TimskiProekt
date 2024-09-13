63bbe89e96d98a564cf294906547fedf9a4fa52b
Viktor Tasevski <viktor-tasevski@hotmail.com>
2024-09-13 10:10:24 +0200

    updates

--- public/utils/DestinationComponent.js
+++
@@ -1,7 +0,0 @@
-export default class DestinationComponent {
-    constructor(path, component, exact = false) {
-        this.path = path;
-        this.component = component;
-        this.exact = exact;
-    }
-}
--- public/utils/onClickRouting.js
+++
@@ -1,9 +0,0 @@
-export default function onClickRouting(path, history) {
-    const currentPath = history.location.pathname;
-    if (currentPath === path) {
-        history.replace(path);
-        return;
-    }
-    history.push(path);
-};
-
